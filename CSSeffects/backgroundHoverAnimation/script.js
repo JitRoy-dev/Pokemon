a7a63b0ed887832dcf8ec712366ab16f48ec8e39
Jit Roy <jitroy287@gmail.com>
2024-10-08 18:11:52 +0530

    new

---
+++ CSSeffects/backgroundHoverAnimation/script.js
@@ -0,0 +1,9 @@
+let container = document.querySelector('.container')
+
+for(let i=1; i <= 2500; i++){
+    let box = document.createElement('div');
+    box.classList.add("box");
+    container.appendChild(box);
+
+
+}
